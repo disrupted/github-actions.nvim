fc6acc8ad52f89c99384fecfd324bbbc6c3046dd
disrupted <hi@salomonpopp.me>
2024-12-20 14:26:02 +0100

    feat: add Treesitter parsers

---
+++ lua/github-actions/init.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.setup = function(opts)
+  require('github-actions.treesitter')
+end
+
+return M
---
+++ lua/github-actions/treesitter.lua
@@ -0,0 +1,40 @@
+-- for development
+local ft = 'gh_vars' -- GitHub actions variables
+vim.filetype.add({
+  extension = {
+    [ft] = ft,
+  },
+})
+
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+local lang_name = 'githubactions'
+parser_config[lang_name] = {
+  maintainers = { 'disrupted' },
+  filetype = ft,
+  install_info = {
+    url = '~/dev/tree-sitter-github-actions',
+    files = { 'src/parser.c' },
+  },
+}
+require('nvim-treesitter.install').ensure_installed_sync(lang_name)
+
+-- lang_name = 'githubactionscondition'
+-- parser_config[lang_name] = {
+--   maintainers = { 'disrupted' },
+--   install_info = {
+--     url = '~/dev/tree-sitter-github-actions-condition',
+--     files = { 'src/parser.c' },
+--   },
+-- }
+-- require('nvim-treesitter.install').ensure_installed_sync(lang_name)
+
+lang_name = 'githubbash'
+parser_config[lang_name] = {
+  maintainers = { 'disrupted' },
+  filetype = lang_name,
+  install_info = {
+    url = '~/dev/tree-sitter-github-bash',
+    files = { 'src/parser.c', 'src/scanner.c' },
+  },
+}
+require('nvim-treesitter.install').ensure_installed_sync(lang_name)
